e781fadf2c8e63bb5326b7336d7606eca7e727ee
KushagraVarshney101 <114856143+KushagraVarshney101@users.noreply.github.com>
2023-02-27 22:31:26 +0530

    Add files via upload

---
+++ profit or loss1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    float C, S;
+    scanf("%f", &C);
+    scanf("%f", &S);
+    if (C > S){
+        printf("Loss\n%.2f%%", (C - S)/ C * 100);
+    }
+    else{
+        printf("Profit\n%.2f%%", (S - C) /C * 100 );
+    }
+    return 0;
+}
